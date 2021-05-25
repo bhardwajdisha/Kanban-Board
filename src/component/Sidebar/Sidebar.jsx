28e0abc467e5bf593d24121962ed4a765b95087d
Disha Bhardwaj <disha2.bhardwaj@gmail.com>
2021-05-25 13:40:28 +0530

    Made sideNavbar responsive

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState ,useEffect} from 'react'
 import {Link } from 'react-router-dom'
 
 import {FaArtstation} from 'react-icons/fa'
@@ -7,40 +7,71 @@ import Button from 'react-bootstrap/Button';
 import {sideNavData} from './sideNavData'
 
 import './Sidebar.scss'
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const SideBar = () => {
-
     const [selected,setIsSelected] = useState()
-    return (
-        <div className='sideNav'>
-            <div className="sideNav-header">
-                <Link to='#'> 
-                    <FaArtstation />
-                    <span> TeamSpace</span>
-                </Link>
-            </div>
-            <div className="sideNav-contents ">
-                <ul className="nav-list">
-                    {
-                        sideNavData.map((item,index)=>(
-                            <li key={index} className={ `${item.cName} ${selected === index?'nav-active': ''}`} onClick={()=>setIsSelected(index)}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Link>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
+    const [width, setWidth] = useState(window.innerWidth);
 
-            <div className="upgrade-plan">
-                <p className="trail">Currently in Trail</p>
-                <p className="days-left">Trail: 17 days left</p>
-                <Button variant="light" className="btn">Upgrade plan</Button>
+    useEffect(() => {
+        function handleResize() {
+        setWidth(window.innerWidth);
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [width]);
+
+    useEffect(() => {
+        width < 900 && handleSideNavToggle();
+    },[width]);
+
+    function handleSideNavToggle() {
+        console.log("toggle it");
+    }
+
+    return (
+        <nav className='sideNav'>
+            <div className="sideNav-header">
+                <div className="heading">
+                    <Link to='#'> 
+                        <FaArtstation />
+                        <span> TeamSpace</span>
+                    </Link> 
+                </div>
+                {width < 900 && <div className="ToggleButton">
+                    <button
+                        className="navbar-toggler"  
+                        type="button" 
+                        data-toggle="collapse" 
+                        data-target="#navbarSupportContent" 
+                        aria-controls="navbarSupportContent" aria-expanded="true" aria-label="Toggle navigation"
+                    >
+                        <GiHamburgerMenu />
+                    </button>
+                </div>}
             </div>
-           
-        </div>
+            <div className={` ${width < 900?'collapse navbar-collapse':''}`} id="navbarSupportContent">
+                <div className=" nav-items sideNav-contents">
+                    <ul className=" nav-list ">
+                        {
+                            sideNavData.map((item,index)=>(
+                                <li key={index} className={ ` ${item.cName} ${selected === index?'nav-active': ''}`} onClick={()=>setIsSelected(index)}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            ))
+                        } 
+                    </ul>
+                </div>
+                <div className="upgrade-plan nav-items ">
+                    <p className="trail">Currently in Trail</p>
+                    <p className="days-left">Trail: 17 days left</p>
+                    <Button variant="light" className="btn">Upgrade plan</Button>
+                </div>
+            </div>
+        </nav>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import $ from'jquery';
+import Popper from'popper.js';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
